dad42f2b4e5048fc6dd2aace072d87aa309e07e8
peocloo <philemoneyramocloo@gmail.com>
2024-01-03 02:51:54 +0000

    2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(97, 123):
-    print(chr(i), end = "")
+#for i in range(97, 123):
+#   print("{i}", end = "").format[chr(i)]
+print(''.join(chr(i) for i in range(97, 123)), end = "")
