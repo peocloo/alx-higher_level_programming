46f940bb744a2c57450ca1489e1ee8453353b7c0
peocloo <philemoneyramocloo@gmail.com>
2024-03-03 16:41:17 +0000

    safe print 2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     num = 0
-    try:
-        for item in range(x):
-            print("{}".format(my_list[item]), end='')
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
             num += 1
         except IndexError:
-            pass
-        print()
-        return num
+            break
+    print("")
+    return num
